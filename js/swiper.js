0aec17de1b24065b877f19531d7df2f5a69d6d1a
alesanchezrey <alesanchezrey@gmail.com>
2023-03-10 20:05:07 -0400

    Deportes y Contacto modificados

--- js/swiper.js
+++ js/swiper.js
@@ -14,6 +14,10 @@ var swiper = new Swiper(".swiper", {
       el: ".swiper-pagination",
       clickable: true
     },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
     breakpoints: {
       640: {
         slidesPerView: 2
